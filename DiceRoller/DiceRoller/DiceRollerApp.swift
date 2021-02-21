0f1bdb2f236bbcca4d3f3081e1c0b54e6acb4cb2
Dave Spina <dspina79@gmail.com>
2021-02-20 20:48:40 -0500

    Project initialization

---
+++ DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Dave Spina on 2/20/21.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
